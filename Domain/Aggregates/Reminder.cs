2056c39dfce5773641a97061da7e13a190ea18c7
ΔΗΜΗΤΡΙΟΣ ΚΟΣΜΑΣ <dimitkos@yahoo.gr>
2024-10-12 23:21:27 +0300

    Add project files.

---
+++ Domain/Aggregates/Reminder.cs
@@ -0,0 +1,59 @@
+﻿namespace Domain.Aggregates
+{
+    public class Reminder
+    {
+        public int Id { get; }
+        public string Message { get; private set; }
+        public Channel Channel { get; private set; }
+        public DateTime CreatedAt { get; }
+        public DateTime NotifyAt { get; private set; }
+
+        public Reminder(int id, string message, Channel channel, DateTime createdAt, DateTime notifyAt)
+        {
+            Id = id;
+            Message = message;
+            Channel = channel;
+            CreatedAt = createdAt;
+            NotifyAt = notifyAt;
+        }
+
+        public static Reminder CreateReminder(int id, string message, Channel channel, DateTime notifyAt)
+        {
+            return new Reminder(
+                id: id,
+                message: message,
+                channel: channel,
+                createdAt: DateTime.UtcNow,
+                notifyAt: notifyAt);
+        }
+
+        public Reminder UpdateMessage(string message)
+        {
+            Message = message;
+
+            return this;
+        }
+
+        public Reminder UpdateChannel(Channel channel)
+        {
+            Channel = channel;
+
+            return this;
+        }
+
+        public Reminder UpdateNotifyAt(DateTime notifyAt)
+        {
+            NotifyAt = notifyAt;
+
+            return this;
+        }
+    }
+
+#warning move this to shared
+    public enum Channel
+    {
+        EmailMessage,
+        MobileMessage,
+        Notification
+    }
+}
---
+++ Domain/Aggregates/User.cs
@@ -0,0 +1,104 @@
+﻿namespace Domain.Aggregates
+{
+    public class User
+    {
+        public int Id { get; }
+        public string Firstname { get; }
+        public string Lastname { get; }
+        public string Email { get; private set; }
+        public string Mobile { get; private set; }
+        public DateTime RegisteredAt { get; }
+        public List<Reminder> Reminders { get; }
+
+        public User(int id, string firstname, string lastname, string email, string mobile, DateTime registeredAt, List<Reminder> reminders)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            Email = email;
+            Mobile = mobile;
+            RegisteredAt = registeredAt;
+            Reminders = reminders;
+        }
+
+        public static User CreateUser(int id, string firstname, string lastname, string email, string mobile)
+        {
+            return new User(
+                id: id,
+                firstname: firstname,
+                lastname: lastname,
+                email: email,
+                mobile: mobile,
+                registeredAt: DateTime.UtcNow,
+                reminders: new List<Reminder>());
+        }
+
+        public User UpdateEmail(string email)
+        {
+            Email = email;
+
+            return this;
+        }
+
+        public User CreateReminder(string email)
+        {
+            Email = email;
+
+            return this;
+        }
+
+        public User UpdateChannelReminder(int reminderId, Channel channel)
+        {
+            var reminder = Reminders.FirstOrDefault(x => x.Id == reminderId);
+
+            if (reminder is null)
+                throw new Exception($"Reminder with id {reminderId} does not exist");
+
+            var updatedRemider = reminder.UpdateChannel(channel);
+
+            reminder = updatedRemider;
+
+            return this;
+        }
+
+        public User UpdateMessageReminder(int reminderId, string message)
+        {
+            var reminder = Reminders.FirstOrDefault(x => x.Id == reminderId);
+
+            if (reminder is null)
+                throw new Exception($"Reminder with id {reminderId} does not exist");
+
+            var updatedRemider = reminder.UpdateMessage(message);
+
+            reminder = updatedRemider;
+
+            return this;
+        }
+
+        public User UpdateMessageNotifyAt(int reminderId, DateTime notifyAt)
+        {
+            var reminder = Reminders.FirstOrDefault(x => x.Id == reminderId);
+
+            if (reminder is null)
+                throw new Exception($"Reminder with id {reminderId} does not exist");
+
+            var updatedRemider = reminder.UpdateNotifyAt(notifyAt);
+
+            reminder = updatedRemider;
+
+            return this;
+        }
+
+        public User DeleteReminder(int reminderId)
+        {
+            var reminder = Reminders.FirstOrDefault(x => x.Id == reminderId);
+
+            if (reminder is null)
+                throw new Exception($"Reminder with id {reminderId} does not exist");
+
+            Reminders.Remove(reminder);
+
+            return this;
+        }
+    }
+}
