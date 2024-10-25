96fc0281907b3c6570b790e177194e9a802faac9
ΔΗΜΗΤΡΙΟΣ ΚΟΣΜΑΣ <dimitkos@yahoo.gr>
2024-10-25 22:06:31 +0300

    Add domain events, di fixes

--- Infrastructure/AutofacModule.cs
+++ Infrastructure/AutofacModule.cs
@@ -1,6 +1,33 @@
-﻿namespace Infrastructure
+﻿using Application.Services.Infrastructure;
+using Application.Services.Infrastructure.Reminders;
+using Application.Services.Infrastructure.Users;
+using Autofac;
+using Infrastructure.Persistence.Commands.Reminders;
+using Infrastructure.Persistence.Commands.Users;
+using System.Reflection;
+
+namespace Infrastructure
 {
     public class AutofacInfrastructureModule : Autofac.Module
     {
+        protected override void Load(ContainerBuilder builder)
+        {
+            var thisAssembly = Assembly.GetExecutingAssembly();
+
+            builder
+                .RegisterType<UserCommandRepository>()
+                .As<IUserCommandRepository>()
+                .SingleInstance();
+
+            builder
+                .RegisterType<ReminderCommandRepository>()
+                .As<IReminderCommandRepository>()
+                .SingleInstance();
+
+            builder
+                .RegisterAssemblyTypes(thisAssembly)
+                .AsClosedTypesOf(typeof(IDomainRetrievalRepository<,>))
+                .SingleInstance();
+        }
     }
 }
