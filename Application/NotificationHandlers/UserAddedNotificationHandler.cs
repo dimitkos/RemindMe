20eddba3e0acb69febb9f659fefd36fbbe8b3791
ΔΗΜΗΤΡΙΟΣ ΚΟΣΜΑΣ <dimitkos@yahoo.gr>
2024-10-25 22:34:35 +0300

    ttt

--- Application/NotificationHandlers/UserAddedNotificationHandler.cs
+++ Application/NotificationHandlers/UserAddedNotificationHandler.cs
@@ -7,7 +7,7 @@ namespace Application.NotificationHandlers
     {
         public Task Handle(UserAdded notification, CancellationToken cancellationToken)
         {
-            //todo2
+            //todo3
             throw new NotImplementedException();
         }
     }
